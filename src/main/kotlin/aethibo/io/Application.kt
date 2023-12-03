659684f97ae81d02c15ddac0e20a88964814e3bd
Karić Kenan <kenan.karic@outlook.com>
2023-12-03 09:57:31 +0100

    Init project

---
+++ src/main/kotlin/aethibo/io/Application.kt
@@ -0,0 +1,12 @@
+package aethibo.io
+
+import aethibo.io.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.cio.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
