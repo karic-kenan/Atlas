11893b275c1930ede6f77436a19b8cf4f0b5a5a6
Karić Kenan <kenan.karic@outlook.com>
2025-09-09 15:50:45 +0200

    ATLAS-DEV-04-001 | Database Connection Modernization (#26)

--- src/main/kotlin/io/aethibo/core/config/Databases.kt
+++ src/main/kotlin/io/aethibo/core/config/Databases.kt
@@ -1,5 +1,8 @@
 package io.aethibo.core.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import com.zaxxer.hikari.metrics.micrometer.MicrometerMetricsTrackerFactory
 import io.aethibo.features.articles.data.model.ArticleEntity
 import io.aethibo.features.articles.data.model.ArticleTagsEntity
 import io.aethibo.features.articles.data.model.FavoritesEntity
@@ -8,31 +11,55 @@ import io.aethibo.features.tags.data.model.TagEntity
 import io.aethibo.features.users.data.model.FollowsEntity
 import io.aethibo.features.users.data.model.UserEntity
 import io.ktor.server.application.*
+import io.micrometer.core.instrument.MeterRegistry
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.koin.ktor.ext.get
 
 fun Application.configureDatabases() {
-    val config = environment.config
+    val config = environment.config.config("database")
 
-    // Database connection setup
-    val driver = config.property("database.driver").getString()
-    val url = config.property("database.url").getString()
-    val username = config.propertyOrNull("database.username")?.getString() ?: ""
-    val password = config.propertyOrNull("database.password")?.getString() ?: ""
+    val hikariConfig = HikariConfig().apply {
+        driverClassName = config.property("driver").getString()
+        jdbcUrl = config.property("url").getString()
+        username = config.propertyOrNull("username")?.getString()
+        password = config.propertyOrNull("password")?.getString()
 
-    Database.connect(url, driver, username, password)
+        maximumPoolSize = config.propertyOrNull("maximumPoolSize")?.getString()?.toInt() ?: 10
+        minimumIdle = config.propertyOrNull("minimumIdle")?.getString()?.toInt() ?: 2
+        idleTimeout = config.propertyOrNull("idleTimeout")?.getString()?.toLong() ?: 60_000
+        maxLifetime = config.propertyOrNull("maxLifetime")?.getString()?.toLong() ?: (30 * 60 * 1000)
+        connectionTimeout = config.propertyOrNull("connectionTimeout")?.getString()?.toLong() ?: 10_000
 
-    // Schema creation
-    transaction {
-        SchemaUtils.run {
-            create(UserEntity)
-            create(FollowsEntity)
-            create(ArticleEntity)
-            create(FavoritesEntity)
-            create(ArticleTagsEntity)
-            create(CommentEntity)
-            create(TagEntity)
-        }
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
     }
+
+    val dataSource = HikariDataSource(hikariConfig)
+    Database.connect(dataSource)
+
+    transaction {
+        SchemaUtils.create(
+            UserEntity,
+            FollowsEntity,
+            ArticleEntity,
+            FavoritesEntity,
+            ArticleTagsEntity,
+            CommentEntity,
+            TagEntity
+        )
+    }
+
+    monitor.subscribe(ApplicationStopping) {
+        dataSource.close()
+    }
+
+    monitorHikari(dataSource)
+}
+
+fun Application.monitorHikari(dataSource: HikariDataSource) {
+    val meterRegistry = get<MeterRegistry>()
+    dataSource.metricRegistry = null
+    dataSource.metricsTrackerFactory = MicrometerMetricsTrackerFactory(meterRegistry)
 }
